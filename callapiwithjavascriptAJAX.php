a4726980f56ee18faeb8669178bb918d2c2963a6
MFaqihMunandar <123913563+MFaqihMunandar@users.noreply.github.com>
2023-02-08 09:33:44 +0700

    Update callapiwithjavascriptAJAX.php

--- callapiwithjavascriptAJAX.php
+++ callapiwithjavascriptAJAX.php
@@ -6,29 +6,38 @@
 	<body>
 		<a id='testajax' name='testajax' onclick='ajaxCall();' > get json </a>
 		<script>
-			function ajaxCall() {
-				$.ajax({
-					// Our sample url to make request can be a php file, json, or else 
-					url: 'http://192.168.1.9:8080/sboservice/groupkredit3.php',
-   					// put your request param inside this data
-					data: {name:'yogesh',salary: 35000,email: 'yogesh@makitweb.com'},
-					// put your request author, signaature or anything else inside this header
-					headers:{         
-						'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBWZXIiOiIwLjAuMCIsImV4cCI6NDcyNjM4OTEyMiwibG9jYWxlIjoiIiwibWFzdGVyVmVyIjoiIiwicGxhdGZvcm0iOiIiLCJwbGF0Zm9ybVZlciI6IiIsInVzZXJJZCI6IiJ9.QIZbmB5_9Xlap_gDhjETfMI6EAmR15yBtIQkWFWJkrg',
-					},
-					// Type of Request
-					type: "GET",
-					// Function to call when to
-					// request is ok 
-					success: function (data) {
-						const p = data;
-						alert(JSON.stringify(p));
-					},
-					// Error handling 
+			function ajaxCall(a,b,c,d,e,f,g,h,i,j,k){
+				var Book = {	value_a:a,
+						value_b:b,
+						value_c:c,
+						value_d:d,
+						value_e:e,
+						value_j:j,
+						value_f:f,
+						value_k:k,
+						value_g:g,
+						value_h:h,
+						value_i:i,
+					    	//this is how you include from php, if only the value stay the same
+						userid:"<?php echo $_SESSION['userid'];?>",
+						uoffice:"<?php echo $_SESSION['uoffice'];?>"
+					}
+													
+				$.ajax(
+					{
+						url:'someserver.php',
+						type: "POST",
+						data:JSON.stringify( Book ),
+						contentType: "application/json; charset=utf-8",
+						dataType: "json",
+						success: function (data) {
+							//do whatever inside here if it success 
+						},
 						error: function (error) {
 							console.log(`Error ${error}`);
 						}
-					});
+					}
+				);
 			}
 		</script>
 	</body>
